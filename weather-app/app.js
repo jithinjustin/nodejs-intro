0e0919c110c0817a83dd29435659db69ad8ee058
jithin <jithinjustin@gmail.com>
2020-10-09 15:04:24 -0400

    add geocoding

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,21 @@
-console.log('starting')
+const request = require('request')
 
-setTimeout(()=>{
-    console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=token1&query=37.8267,-122.4233&units=f';
 
-setTimeout(()=>{
-    console.log('0 second timer')
-}, 0)
+request({ url: url, json: true},(error, response)=>{
+console.log(response.body.current.weather_descriptions[0]+" "+
+    response.body.current.temperature)
 
-console.log('Stopping')
+
+})
+
+const mapUrl= 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=token2&limit=1'
+request({url:mapUrl, json:true},(error, response)=>{
+ 
+ 
+   // console.log(response.body.features[0].center)
+   const longitude = response.body.features[0].center[0]
+
+   const latitude = response.body.features[0].center[1]
+   console.log(latitude, longitude)
+})
